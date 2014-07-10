3ae6806c94814c74c74451c233994d9e16254889
UmedaTakefumi <ut@azurestone.net>
2014-07-11 01:36:51 +0900

    Add:  A Tour of Go の1を開始
      * 日本語版：
        * http://go-tour-jp.appspot.com/#1
      * 英語版：
        * http://tour.golang.org/#1

---
+++ 01/hello.go
@@ -0,0 +1,10 @@
+//usr/bin/env go run $0 $@ ; exit                                                                                                                                                     
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Printf("Hello, 世界\n")
+}
+
