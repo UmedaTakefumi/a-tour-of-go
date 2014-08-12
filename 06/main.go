dc6ef914c147a8cf4b958e5a52df3ed9fd2ae3f6
UmedaTakefumi <ut+github@azurestone.net>
2014-08-13 08:47:02 +0900

    Add:  A Tour of Go の6を追加
      * 日本語版：
        * http://go-tour-jp.appspot.com/#6
      * 英語版：
        * http://tour.golang.org/#6

---
+++ 06/main.go
@@ -0,0 +1,13 @@
+//usr/bin/env go run $0 $@ ; exit                                                                                                                                                     
+
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+func main() {
+  fmt.Println(math.Pi)
+}
+
