f87d1adfa4e0951711a98969f195d153dcc437ea
UmedaTakefumi <ut+github@azurestone.net>
2014-08-18 23:42:47 +0900

    Fix:  A Tour of Go の9のソースコードを修正
      間違えて A Tour of Go の10のソースコードを掲載していた

--- 09/main.go
+++ 09/main.go
@@ -4,13 +4,13 @@ package main
 
 import "fmt"
 
-func split(sum int) (x, y int) {
-  x = sum * 4 / 9
-  y = sum - x
-  return 
+func swap(x, y string) (string, string) {
+  return y, x
 }
 
 func main() {
-  fmt.Println(split(17))
+  a, b := swap("hello", "world")
+  fmt.Println(a, b)
 }
+
 
